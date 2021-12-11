c0862e618c620c59dac90052e2b42ca6a04a23e1
hellcy <chengyuan82281681@hotmail.com>
2021-12-11 22:10:37 +1100

    added uuid for new Activity

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-import { Container, Header, List } from 'semantic-ui-react'
+import { Container } from 'semantic-ui-react'
 import { Activity } from '../models/activity'
 import NavBar from './NavBar'
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard'
+import { v4 as uuid } from 'uuid'
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([])
@@ -43,7 +44,7 @@ function App() {
           ...activities.filter((x) => x.id !== activity.id),
           activity,
         ])
-      : setActivities([...activities, activity])
+      : setActivities([...activities, { ...activity, id: uuid() }])
 
     setEditMode(false)
     setSelectedActivity(activity)
