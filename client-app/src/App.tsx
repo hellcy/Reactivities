7aa92850bd7977358d4ff22ad4a69ddbaa087d1a
hellcy <chengyuan82281681@hotmail.com>
2021-12-11 12:43:34 +1100

    added Semantic UI

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,34 +1,29 @@
-import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import axios from 'axios'
+import { Header, List } from 'semantic-ui-react'
 
 function App() {
-  const [activities, setActivities] = useState([]);
+    const [activities, setActivities] = useState([])
 
-  useEffect(() => {
-    axios.get('http://localhost:5000/api/activities').then(response => {
-      console.log(response);
-      setActivities(response.data);
-    })
-  }, [])
+    useEffect(() => {
+        axios.get('http://localhost:5000/api/activities').then((response) => {
+            console.log(response)
+            setActivities(response.data)
+        })
+    }, [])
 
-
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <ul>
-          {activities.map((activity : any) => (
-            <li key={activity.id}>
-              {activity.title}
-            </li>
-          ))}
-        </ul>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Header as="h2" icon="users" content="Reactivities" />
+            <List>
+                {activities.map((activity: any) => (
+                    <List.Item key={activity.id}>{activity.title}</List.Item>
+                ))}
+            </List>
+        </div>
+    )
 }
 
-export default App;
+export default App
